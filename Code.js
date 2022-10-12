95595f94f25db9fffcc86f06d082d50a4af7f2da
Johan-MG <johanmg902@gmail.com>
2022-10-11 20:13:31 -0500

    Agregando la parte de gabo

--- Code.js
+++ Code.js
@@ -1,11 +1,18 @@
 
 var dinero;
 const denominaciones = [1000, 500, 200, 100, 50, 20];
+var antes, despues;
+var aux;
+despues = [0,0,0,0,0,0];
+var Nsimulacion = 0;
+var sinDinero = true;
 
 // Devuelve los valores a su estado inicial
 function reiniciar(){
     // El orden de los billetes es: 1000, 500, 200, 100, 50, 20
     dinero = [100, 100, 100, 100, 100, 100];
+    Nsimulacion = 0;
+    sinDinero = true;
 }
 
 reiniciar();
@@ -13,21 +20,28 @@ reiniciar();
 // Función para retirar dinero
 function retiro(cantidad){
     // El for realiza una busqueda por cada una de las denominaciones
+    
     for(let i = 0; i<6; i++){
+
         //Si la cantidad solicitada es mayor o igual a una denominacion entonces retira un billete de esa denominación y compara nuevamente
         do {
             if(cantidad>=denominaciones[i]){
-                if(dinero[i]>0){
-                    dinero[i]--;
-                    cantidad -= denominaciones[i];
-                }else{
+                if((cantidad==60 || cantidad == 80) && i==4){
                     break;
+                }else{
+                    if(dinero[i]>0){
+                        dinero[i]--;
+                        cantidad -= denominaciones[i];
+                    }else{
+                        break;
+                    }
                 }
             }
         } while (cantidad>=denominaciones[i]);
 
         if(i==5 && cantidad>0){
             console.log("Se me acabó el dinero :(");
+            sinDinero = false;
         }
     }
 }
@@ -35,6 +49,7 @@ function retiro(cantidad){
 // Esta función realiza la operación si cumple las condiciones, de otra manera lanza un mensaje de error
 function solicitud(cantidad){
     if(verificacion(cantidad)){
+        antes = dinero.slice();
         retiro(cantidad);
     }else{
         console.log("La cantidad ingresada es incorrecta");
@@ -54,10 +69,32 @@ function verificacion(cantidad){
     }
 }
 
-solicitud(6000);
+function compararBilletes(){
+    for(let i=0; i<6; i++){
+        despues[i] = antes[i]-dinero[i];
+    }
+    return despues;
+}
+
+function simular(){
+    do {
+        aux = Math.floor(Math.random() * 200 + 1)*20
+        retiro(aux);
+        Nsimulacion++;
+    } while (sinDinero);
+    console.log(aux);
+}
+
+/* solicitud(2000);
 
 console.log(denominaciones);
 console.log(dinero);
+console.log(compararBilletes()); */
+simular();
+//solicitud(80);
+console.log(dinero);
+console.log(Nsimulacion);
+
 
 
 
